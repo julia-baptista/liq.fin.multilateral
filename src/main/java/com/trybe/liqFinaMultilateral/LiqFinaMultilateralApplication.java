eb688f8c840df4e8a81625b92feacb3cbcdc87c4
Julia Baptista <baptista.julia@yahoo.com.br>
2022-11-17 13:21:19 -0300

    Iniciando o projeto

---
+++ src/main/java/com/trybe/liqFinaMultilateral/LiqFinaMultilateralApplication.java
@@ -0,0 +1,13 @@
+package com.trybe.liqFinaMultilateral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiqFinaMultilateralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiqFinaMultilateralApplication.class, args);
+	}
+
+}
